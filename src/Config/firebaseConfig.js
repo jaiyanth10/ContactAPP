dda27fc7cb5ee41945e8ccce25abdd81d74ad02f
Jaiyanth Lakumarapu <71588852+jaiyanth10@users.noreply.github.com>
2024-05-24 18:16:14 -0400

    Update firebaseConfig.js (no keys)

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";//import this
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCG7Y7301pL9EPh1_cbuZXs9wIOtTdpmhE",
-  authDomain: "contactapp-306f4.firebaseapp.com",
-  projectId: "contactapp-306f4",
-  storageBucket: "contactapp-306f4.appspot.com",
-  messagingSenderId: "928289058432",
-  appId: "1:928289058432:web:1a7dd5798f58cccd640065"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
